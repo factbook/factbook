4800ca75b917993382261c8bdb107a35b626ce7d
Gerald Bauer <gerald.bauer@gmail.com>
2015-11-04 00:16:11 +0100

    up

--- lib/factbook/page.rb
+++ lib/factbook/page.rb
@@ -86,24 +86,89 @@ class Page
 
   ATTRIBUTES = {
    'Introduction' => [[:background, 'Background' ]],
-   'Geography'    => [[:area,             'Area', 'total'],    ## convert to number -- why? why not??
+   'Geography'    => [[:location,         'Location'],
+                      [:geo_coords,       'Geographic coordinates'],   ## just use coords - why, why not??
+                      [:map_ref,          'Map references'],  ## just use map - why, why not??
+                      [:area,             'Area', 'total'],
                       [:area_land,        'Area', 'land' ],
                       [:area_water,       'Area', 'water'],
                       [:area_note,        'Area', 'note' ],
                       [:area_comparative, 'Area - comparative'],
+                      [:border_land,      'Land boundaries', 'total'],
+                      [:border_countries, 'Land boundaries', 'border countries'],   ## todo/fix: remove count from key e.g. border countries (8)
+                      [:coastline,        'Coastline'],   ## use border_water  - why, why not??
+                      [:maritime_claims,  'Maritime claims'],
                       [:climate,          'Climate'],
                       [:terrain,          'Terrain'],
                       [:elevation_lowest, 'Elevation extremes', 'lowest point'],
                       [:elevation_highest,'Elevation extremes', 'highest point'],
-                      [:resources,        'Natural resources']],
-  'People and Society' => [[:languages,         'Languages' ],
+                      [:resources,        'Natural resources'],
+                      [:land_use_agriculture, 'Land use', 'agricultural land'],
+                      [:land_use_forest,      'Land use', 'forest'],
+                      [:land_use_other,       'Land use', 'other'],
+                      [:land_irrigated,       'Irrigated land'],
+                      [:water_renewable,      'Total renewable water resources'],
+                      [:water_withdrawal,     'Freshwater withdrawal (domestic/industrial/agricultural)', 'total'],
+                      [:water_withdrawal_capita, 'Freshwater withdrawal (domestic/industrial/agricultural)', 'per capita'],
+                      [:natural_hazards,    'Natural hazards'],
+                      [:environment_issues,          'Environment - current issues'],
+                      [:environment_agreements,      'Environment - international agreements', 'party to'],
+                      [:environment_agreements_note, 'Environment - international agreements', 'signed, but not ratified'],   ## use _ratified - why, why not??
+                      [:geo_note,                    'Geography - note']],
+  'People and Society' => [[:nationality,           'Nationality', 'noun'],
+                           [:nationality_adjective, 'Nationality', 'adjective'],
+                           [:ethnic_groups,     'Ethnic groups'],
+                           [:languages,         'Languages' ],
                            [:religions,         'Religions' ],
                            [:population,        'Population' ],
+                           [:age_0_14_yrs,          'Age structure', '0-14 years'],
+                           [:age_15_24_yrs,         'Age structure', '15-24 years'],
+                           [:age_25_54_yrs,         'Age structure', '25-54 years'],
+                           [:age_55_64_yrs,         'Age structure', '55-64 years'],
+                           [:age_65_plus_yrs,       'Age structure', '65 years and over'],
+                           [:dependency_ratio,       'Dependency ratios', 'total'],
+                           [:dependency_youth_ratio,  'Dependency ratios', 'youth dependency ratio'],
+                           [:dependency_elderly_ratio, 'Dependency ratios', 'elderly dependency ratio'],
+                           [:dependency_support_ratio, 'Dependency ratios', 'potential support ratio'],
+                           [:median_age,        'Median age', 'total'],
+                           [:median_age_male,   'Median age', 'male'],
+                           [:median_age_female, 'Median age', 'female'],                           
                            [:population_growth, 'Population growth rate' ],
                            [:birth_rate,        'Birth rate' ],
                            [:death_rate,        'Death rate' ],
                            [:migration_rate,    'Net migration rate' ],
-                           [:major_cities,      'Major urban areas - population' ]],
+                           [:urbanization,      'Urbanization', 'urban population'],
+                           [:urbanization_growth, 'Urbanization', 'rate of urbanization'], ## use _rate - why, why not??
+                           [:major_cities,      'Major urban areas - population' ],
+                           [:sex_ratio_birth,     'Sex ratio', 'at birth'],
+                           [:sex_ratio_0_14_yrs,  'Sex ratio',  '0-14 years'],
+                           [:sex_ratio_15_24_yrs, 'Sex ratio', '15-24 years'],
+                           [:sex_ratio_25_54_yrs, 'Sex ratio', '25-54 years'],
+                           [:sex_ratio_55_64_yrs, 'Sex ratio', '55-64 years'],
+                           [:sex_ratio_65_plus_yrs, 'Sex ratio', '65 years and over'],
+                           [:sex_ratio,             'Sex ratio', 'total population'],
+                           [:infant_mortality,      'Infant mortality rate', 'total'],  ## add _rate - why, why not??
+                           [:infant_mortality_male,   'Infant mortality rate', 'male'],
+                           [:infant_mortality_female, 'Infant mortality rate', 'female'],
+                           [:life_expectancy,         'Life expectancy at birth', 'total population'],
+                           [:life_expectancy_male,    'Life expectancy at birth', 'male'],
+                           [:life_expectancy_female,  'Life expectancy at birth', 'female'],
+                           [:fertility,               'Total fertility rate'],
+                           [:contraceptive_rate,      'Contraceptive prevalence rate'],
+                           [:contraceptive_rate_note, 'Contraceptive prevalence rate', 'note'],
+                           [:health_expenditures,     'Health expenditures'],  ## todo: change to health ??
+                           [:density_physicians,      'Physicians density'],
+                           [:density_hospital_bed,    'Hospital bed density'],
+                           [:drinking_water_improved,   'Drinking water source', 'improved'],  ## todo:split into urban/rural/total ??
+                           [:drinking_water_unimproved, 'Drinking water source', 'unimproved'],
+                           [:sanitation_improved,         'Sanitation facility access', 'improved'],  ## todo:split into urban/rural/total ??
+                           [:sanitation_unimproved,       'Sanitation facility access', 'unimproved'],
+                           [:aids,                       'HIV/AIDS - adult prevalence rate'],
+                           [:aids_people,                'HIV/AIDS - people living with HIV/AIDS'],
+                           [:aids_deaths,                'HIV/AIDS - deaths'],
+                           [:obesity,                    'Obesity - adult prevalence rate'],
+                           [:education_expenditures,     'Education expenditures'],  ## change to education ??
+                           ],  
   }
   
   ATTRIBUTES.each do |section_title, attribs|
